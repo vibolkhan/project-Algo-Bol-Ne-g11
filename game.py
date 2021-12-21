5a373558761da1f3dab57e4136a4f357c90dc57b
vibolkhan <vibol.khan@student.passerellesnumeriques.org>
2021-12-21 19:57:24 +0700

    display grid

---
+++ game.py
@@ -0,0 +1,121 @@
+import tkinter as tk
+
+root = tk.Tk() 
+root.geometry("610x610")
+frame = tk.Frame()
+frame.master.title('Amazing game')
+canvas = tk.Canvas(frame)
+
+myBackground=tk.PhotoImage(file='backg.png')
+avatar = tk.PhotoImage(file='avatar.png')
+monster = tk.PhotoImage(file='monster.png')
+home = tk.PhotoImage(file='home.png')
+key = tk.PhotoImage(file='key.png')
+stair=tk.PhotoImage(file="stair.png")
+heart_image=tk.PhotoImage(file="heart.png")
+grid = [
+    [4,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,2,0,0,0,0],
+    [0,1,0,0,0,3],
+]
+
+def drawGrid():
+    canvas.delete('all')
+    canvas.create_image(210,310, image=myBackground)
+    
+    canvas.create_image(260,510,image=stair)
+    canvas.create_image(260,420,image=stair)
+    canvas.create_image(260,330,image=stair)
+    canvas.create_image(260,240,image=stair)
+    canvas.create_image(-225,150,image=stair)
+    canvas.create_text(250,30,text='Lives: ',font=('Ubuntu',18))
+    canvas.create_image(300,30,image=heart_image)
+    canvas.create_image(340,30,image=heart_image)
+    canvas.create_image(380,30,image=heart_image)
+
+    canvas.create_text(540,30,text='Levels: 1 ',font=('Ubuntu',18))
+
+
+    canvas.create_text(70,30,text='Score: ',font=('Ubuntu',18))
+
+    x=35
+    y=60
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 0:
+                canvas.create_rectangle(x,y,x+90,y+90,fill='',outline='')
+            elif grid[row][col] == 1:
+                canvas.create_image(x+45,y+45,image = avatar,anchor='center')   
+            elif grid[row][col] == 2:
+                canvas.create_image(x+45,y+45,image = monster,anchor='center')      
+            elif grid[row][col] == 3:
+                canvas.create_image(x+45,y+45,image = home,anchor='center')
+            elif grid[row][col] == 4:
+                canvas.create_image(x+45,y+45,image = key,anchor='center')                      
+            x+=90
+        x=35
+        y+=90
+
+drawGrid()
+
+def getRowOf1():
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                index = row
+    return index
+
+def getColOf1():
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                index = col
+    return index
+
+def move_left(event):
+    row = getRowOf1()
+    col = getColOf1()
+    if col != 0:
+        grid[row][col] = 0
+        grid[row][col-1]=1
+    # print(grid)
+        drawGrid()
+def move_right(event):
+    row = getRowOf1()
+    col = getColOf1()
+    if col < len(grid[0])-1:
+        grid[row][col] = 0
+        grid[row][col+1]=1
+    # print(grid)
+        drawGrid()
+
+def move_down(event):
+    row = getRowOf1()
+    col = getColOf1()
+    if row < len(grid)-1:
+        grid[row][col] = 0
+        grid[row+1][col]=1
+    # print(grid)
+        drawGrid()
+
+def move_up(event):
+    row = getRowOf1()
+    col = getColOf1()
+    if row != 0:
+        grid[row][col] = 0
+        grid[row-1][col]=1
+    # print(grid)
+        drawGrid()
+
+root.bind('<Left>',move_left) #### keyboard
+root.bind('<Right>',move_right) #### keyboard
+root.bind('<Down>',move_down) #### keyboard
+root.bind('<Up>',move_up) #### keyboard
+
+canvas.pack(expand=True, fill='both')
+frame.pack(expand=True, fill='both')
+
+root.mainloop()
